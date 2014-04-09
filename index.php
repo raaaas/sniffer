8d9cc25689debc74e2e33bfe506c86a6323970dd
raaaas <rsp_emperor@yahoo.com>
2014-04-09 17:31:47 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+ 
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script type="text/javascript" src="swfobject.js"></script>
+<script type="text/javascript" src="pdf.js"></script>
+<script type="text/javascript" src="http://java.com/js/deployJava.js"></script>
+<script type="text/javascript">
+
+var info = getAcrobatInfo();
+
+$.get('main.php?java=' + deployJava.getJREs()+'&flash='+deconcept.SWFObjectUtil.getPlayerVersion().major+'.'+deconcept.SWFObjectUtil.getPlayerVersion().minor+'.'+deconcept.SWFObjectUtil.getPlayerVersion().rev+'&pdf='+info.acrobat + " " + info.acrobatVersion);// Show all installed JRE versions.
+
+			   // window.location.href = "index.php?width=" + width + "&height=" + height;
+        </script>
+
+    <body>
+ 
+    </body>
+</html>
+
